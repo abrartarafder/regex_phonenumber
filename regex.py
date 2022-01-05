320b952eb61aca6211dc87e169a31e07be1a0f09
abrartarafder <95165049+abrartarafder@users.noreply.github.com>
2022-01-04 20:16:05 -0500

    Update regex.py

--- regex.py
+++ regex.py
@@ -1,11 +1,17 @@
+# re imported for regexpression
 import re
 
-def validate_phone(value):
+# function to check phone number
+def validate_phone():
     ip = input("Enter a phone number: ")
-
+#     regex checker
     phone_regex = re.compile('[+0-9]*(-)*[(]*[0-9]{3}[)]*(-)*[0-9]{3}(-)*[0-9]{4}')
+#     checking match
     phone_match = phone_regex.match(ip)
+   
     if phone_match:
         return "VALID PHONE NUMBER"
     else: 
         return "INVALID PHONE NUMBER"
+    
+print(validate_phone())
